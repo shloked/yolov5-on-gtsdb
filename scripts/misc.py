5f54f389cf9c37efe9c136868d6beb623d245837
Dharmashloka Debashis <shlok@gatech.edu>
2021-08-03 10:24:43 -0700

    Update project

---
+++ scripts/misc.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+import cv2
+from PIL import Image
+import os
+import pickle
+from glob import glob
+from tqdm import tqdm
+import time
+
+
+def main():
+    imgpath = "/home/ubuntu/Shlok/gtsdb/data/TrainIJCNN2013/00000.ppm"
+    # im = cv2.imread(imgpath)
+    # cv2.imshow("image", im)
+    # if cv2.waitKey(0)==27:
+    #     return
+    # im = Image.open(imgpath)
+    # im.show()
+
+if __name__=="__main__":
+    main()
